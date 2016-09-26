6f617fd00acb9129dd9ae5146c930077cf0d7032
Ashi Krishnan <me@ashi.works>
2016-09-26 16:41:00 -0400

    Markdown, wiki formatting.

--- 12-wikistack/wikistack/db.js
+++ 12-wikistack/wikistack/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/wikistack')
 
+const marked = require('marked')
 const Page = db.define('page', {
   title: {
     type: Sequelize.STRING
@@ -15,7 +16,37 @@ const Page = db.define('page', {
   status: {
     type: Sequelize.ENUM('open', 'closed')
   }
+}, {
+  getterMethods: {
+    html() {
+      return new Promise((ok, fail) =>
+                         marked(wikiToMarkdown(this.content),
+                                (err, content) => err ? fail(err) : ok(content)))
+    }
+  }
 })
+
+// wikify(content: String) -> String
+//
+// Convert "[[wiki links]]" within content into markdown-style
+// "[wiki links](wiki_links)".
+//
+// Will also convert "[[some title|with different text]]" into
+// "[with different text](some_title)".
+function wikiToMarkdown(content) {
+  return (content || '').replace(/\[\[([\w\s\d]+)(\|([\w\s\d]+))?\]\]/g,
+                                 (match, title, _, text) => {
+                                   console.log('in rpelace', match, text, title)
+                                   return `[${(text || title).trim()}](${asLink(title)})`
+                                 })
+}
+
+// asLink(title: String) -> String
+//
+// Convert "wiki links" into "wiki_links"
+function asLink(title) {
+  return title.replace(/\s/g, '_')
+}
 
 const User = db.define('user', {
   name: {
@@ -25,5 +56,7 @@ const User = db.define('user', {
     type: Sequelize.STRING
   }
 })
+
+Page.belongsTo(User, {as: 'author'})
 
 module.exports = {db, Page, User}
--- 12-wikistack/wikistack/routes.js
+++ 12-wikistack/wikistack/routes.js
@@ -1,4 +1,4 @@
-const {Page} = require('./db')
+const {Page, User} = require('./db')
 
 module.exports = require('express').Router()
   .get('/:urlTitle?', (req, res, next) =>
@@ -11,20 +11,38 @@ module.exports = require('express').Router()
          // we'll just redirect them to edit mode.
          if (!page && !editing) { return res.redirect(req.url + '?edit') }
 
-         // Create a blank page if no such page exists in the DB.
+         // Create a blank page object if no page exists in the DB yet.
          if (!page) page = Page.build({urlTitle: req.params.urlTitle})
 
-         // Draw the page
-         return res.render(editing? 'edit' : 'page', {page, editing})
+         if (editing)
+           // We don't have to render markdown if we're in edit mode,
+           // so just serve the view.
+           return res.render('edit', {page, editing})
+         else
+           // Render the markdown first, then serve the show view.
+           return page.html.then(html =>
+                                 res.render('page', {html, page, editing}))
        })
        .catch(next))
+  // Redirect /wiki/page to /page
+  .get('/wiki/:urlTitle?', (req, res) => {
+    console.log('redirecting', req.url, 'to', req.params.urlTitle)
+    res.redirect(`/${req.params.urlTitle || ''}`)
+  })
   .post('/:urlTitle?', (req, res, next) =>
-        Page
-        .upsert({
-          title: req.body.title,
-          content: req.body.content,
-          urlTitle: req.params.urlTitle || null,
-        })
+        User.findOrCreate({
+          where: {
+            name: req.body.name,
+            email: req.body.email,
+          }
+        }).then(user =>
+                Page
+                .upsert({
+                  title: req.body.title,
+                  content: req.body.content,
+                  urlTitle: req.params.urlTitle || null,
+                  author: user,                  
+                }))
         .then(ok => res.redirect(req.params.urlTitle || '/'))
         .catch(next))
            
