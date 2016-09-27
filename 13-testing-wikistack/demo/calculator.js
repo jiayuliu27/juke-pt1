f8a568bb84f66766b191a97bf8cf9dbe530d107b
intersim <emily.intersimone@gmail.com>
2016-09-27 11:58:51 -0400

    modified demo code

--- 13-testing-wikistack/demo/calculator.js
+++ 13-testing-wikistack/demo/calculator.js
@@ -1,17 +1,27 @@
-var fs = require('fs');
 var Bluebird = require('bluebird');
+var fs = require('fs');
+
+// fs.writeFile('./log.txt', '1, 2, 3', function(err) {
+//    if (err) console.log(err);
+// })
+
+var writeFile = Bluebird.promisify(fs.writeFile);
 
 var Calculator = function() {
 	this.output = [];
 };
 
-Calculator.prototype.add = function(a, b) {
-	var results = a+b;
-	this.output.push(results);
-	return results;
+Calculator.prototype.add = function(n1, n2) {
+	var result = n1 + n2;
+	this.output.push(result);
+	return result;
 };
 
-var writeFile = Bluebird.promisify(fs.writeFile);
+Calculator.prototype.subtract = function(n1, n2) {
+	var result = n1 - n2;
+	this.output.push(result);
+	return result;
+};
 
 Calculator.prototype.logOutput = function() {
 	return writeFile('./output.txt', this.output.toString());
--- 13-testing-wikistack/demo/test.js
+++ 13-testing-wikistack/demo/test.js
@@ -1,24 +1,34 @@
-var Calculator = require('./calculator');
 var chai = require('chai');
-var expect = chai.expect;
 chai.should();
-
+var expect = chai.expect;
 var fs = require('fs');
 
-describe('Calculator', function(){
+var Calculator = require('./calculator.js');
+
+describe('Calculator', function () {
 	var calc;
 
-	beforeEach(function(){
-		calc = new Calculator()
+	beforeEach(function () {
+		calc = new Calculator();
 	});
 
-	describe('add method', function(){
-		it('is a function', function(){
+	describe('add method', function () {
+
+		it('should exist', function () {
+			// example without using chai:
+			// if (typeof calc.add !== 'function') {
+			// 	throw new Error('add is not a function!');
+			// }
+
+			// example of making assertions using 'should' instead of 'expect'
+			// calc.add.should.be.a('function');
+
 			expect(calc.add).to.be.a('function');
+
 		});
 
-		it('adds two numbers together accurately', function(){
-			expect(calc.add(2,2)).to.equal(4);
+		it('should correctly add two numbers', function() {
+			expect(calc.add(2, 2)).to.be.equal(4);
 		});
 
 		it('should correctly put result in output array', function () {
@@ -28,22 +38,44 @@ describe('Calculator', function(){
 		});
 	});
 
-	describe('logOut method', function() {
-		it('should write a file to the filesystem containing the calculator\'s output', function(done){
-			calc.add(1,1);
-			calc.add(6,6);
+	describe('subtract method', function() {
+		it('should exist', function () {
+			expect(calc.subtract).to.be.a('function');
+		});
 
-			calc.logOutput()
-			.then(function(){
-				fs.readFile('./output.txt', function(err, contents){
-					expect(contents.toString()).to.equal('2,12');
-					// done();
-				});
-			});
+		it('should correctly add two numbers', function() {
+			expect(calc.subtract(2, 2)).to.be.equal(0);
+		});
+
+		it('should correctly put result in output array', function () {
+			calc.subtract(3, 1);
+			expect(calc.output).to.have.length(1);
+			expect(calc.output[0]).to.be.equal(2);
 		});
 	});
+
+	describe('logOutput method', function() {
+		afterEach(function(){
+			// this removes the output text file after our tests are done
+			fs.unlink('./output.txt', function(err){
+				if(err) console.error(err);
+			});
+		});
+
+		it('should write a file to the file system with output', function(done) {
+			calc.add(2, 2);
+			calc.add(2, 5);
+			
+			calc.logOutput()
+			.then(function() {
+				fs.readFile('./output.txt', function(err, contents) {
+					expect(contents.toString()).to.be.equal('4,7');
+				});
+				done();
+			});
+
+		})
+	});
+
+
 });
-
-
-
-
